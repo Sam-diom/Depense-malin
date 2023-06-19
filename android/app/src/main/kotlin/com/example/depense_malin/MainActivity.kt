483d36273ce191d0d706bb901afacd423a36a2af
SamDiom <samdiom@192.168.50.49>
2023-06-19 16:40:36 +0000

    project init

---
+++ android/app/src/main/kotlin/com/example/depense_malin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.depense_malin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+// main.dart
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Depense Malin',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            pinned: true,
+            snap: false,
+            centerTitle: false,
+            title: const Text('Depense Malin'),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.list_sharp),
+                onPressed: () {},
+              ),
+            ],
+            bottom: AppBar(
+              title: Container(
+                width: double.infinity,
+                height: 40,
+                color: Colors.white,
+                child: const Center(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Recherchez vos dépenses',
+                      prefixIcon: Icon(Icons.search),
+                      suffixIcon: null,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Other Sliver Widgets
+          SliverList(
+            delegate: SliverChildListDelegate([
+              const SizedBox(
+                height: 400,
+                child: Center(
+                  child: Text(
+                    'Gérez vos dépenses comme un malin 🤣',
+                  ),
+                ),
+              ),
+              Container(
+                height: 1000,
+                color: Colors.blueGrey,
+              ),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
